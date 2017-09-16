c8fe79b1f50b4d7aea437810408fc3bc34b3b904
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-09-15 21:25:25 -0300

    Fix nginx

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-####################
-##### Update all
-####################
-git pull /usr/src/iptvpanel/
+############################################################
+# Update all
+#   bash /usr/src/iptvpanel/update.sh 
+############################################################
+cd /usr/src/iptvpanel/
+git pull
 cp -a /usr/src/iptvpanel/* /usr/local/nginx/html/
 cp /usr/src/iptvpanel/nginx.conf /usr/local/nginx/conf/nginx.conf
 composer install -d /usr/local/nginx/html/
 killall nginx
 /usr/local/nginx/sbin/nginx
+sudo service php7.0-fpm restart
